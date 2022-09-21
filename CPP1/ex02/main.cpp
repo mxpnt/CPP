560e9484c1ac470428dd191f7cd514f2cf26a779
mxpnt <maxime.pontille1@gmail.com>
2022-09-21 11:36:05 +0200

    CPP1/ex02

---
+++ CPP1/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapontil <mapontil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 11:13:26 by mapontil          #+#    #+#             */
+/*   Updated: 2022/09/21 11:32:42 by mapontil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "str adress: " << &str << std::endl;
+	std::cout << "stringPTR adress: " << stringPTR << std::endl;
+	std::cout << "stringREF adress: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "str value: " << str << std::endl;
+	std::cout << "stringPTR value: " << *stringPTR << std::endl;
+	std::cout << "stringREF value: " << stringREF << std::endl;
+	
+	return (0);
+}
