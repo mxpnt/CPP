90b0bca32468a54f39100ce030ebdde1b7683747
mxpnt <maxime.pontille1@gmail.com>
2022-10-25 17:55:17 +0200

    nan handle

--- CPP6/ex00/main.cpp
+++ CPP6/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mapontil <mapontil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 14:35:20 by mapontil          #+#    #+#             */
-/*   Updated: 2022/10/25 17:30:23 by mapontil         ###   ########.fr       */
+/*   Updated: 2022/10/25 17:47:54 by mapontil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 #include <stdexcept>
 #include <cmath>
 
-void	convertChar(double n)
+void	convertChar(double n, std::string str)
 {
 	std::cout << "char: ";
 	if ((n > 0 && n < 33) || n == 127)
 		std::cout << "Non displayable" << std::endl;
-	else if (n > 127 || n > INT_MAX || n < 0 || n < INT_MIN)
+	else if (n > 127 || n > INT_MAX || n < 0 || n < INT_MIN || str == "nan" || str == "nanf")
 		std::cout << "Impossible" << std::endl;
 	else
 	{
@@ -30,10 +30,10 @@ void	convertChar(double n)
 	}
 }
 
-void	convertInt(double n)
+void	convertInt(double n, std::string str)
 {
 	std::cout << "int: ";
-	if (n < INT_MIN || n > INT_MAX)
+	if (n < INT_MIN || n > INT_MAX || str == "nan" || str == "nanf")
 		std::cout << "Impossible" << std::endl;
 	else
 	{
@@ -65,8 +65,8 @@ int	main(int argc, char **argv)
 
 	double	n = std::stod(argv[1]);
 
-	convertChar(n);
-	convertInt(n);
+	convertChar(n, argv[1]);
+	convertInt(n, argv[1]);
 	convertFloat(n);
 	convertDouble(n);
 
