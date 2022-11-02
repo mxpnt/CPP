fb7ec9d5fcd2d7323fe8edce3cc40827d8d36029
mxpnt <maxime.pontille1@gmail.com>
2022-11-02 18:18:56 +0100

    class ex02/CPP8

---
+++ CPP8/ex02/MutantStack.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapontil <mapontil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/02 12:48:57 by mapontil          #+#    #+#             */
+/*   Updated: 2022/11/02 18:18:10 by mapontil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <string>
+# include <stack>
+
+template <typename T>
+class	MutantStack: public std::stack<T> {
+
+};
+
+
+#endif
