97a9d7d95929259a0fc5c8857f46f89185d1f83d
mxpnt <maxime.pontille1@gmail.com>
2022-11-09 09:43:55 +0100

    ex03 clean class.cpp

--- CPP1/ex03/HumanA.cpp
+++ CPP1/ex03/HumanA.cpp
@@ -6,21 +6,13 @@
 /*   By: mapontil <mapontil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 11:53:42 by mapontil          #+#    #+#             */
-/*   Updated: 2022/09/21 13:32:59 by mapontil         ###   ########.fr       */
+/*   Updated: 2022/11/09 09:43:34 by mapontil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-void	HumanA::setWeapon(Weapon type)
-{
-	_Weapon = type;
-}
-
-void	HumanA::attack(void) const
-{
-	std::cout << this->_name << " attacks with their " << _Weapon.getType() << std::endl;
-}
+/****** CONSTRUCTORS AND DESTRUCTOR *****/
 
 HumanA::HumanA(std::string name, Weapon &type): _name(name) ,_Weapon(type)
 {
@@ -30,4 +22,16 @@ HumanA::HumanA(std::string name, Weapon &type): _name(name) ,_Weapon(type)
 HumanA::~HumanA()
 {
 	return ;
+}
+
+/***** MEMBER FUNCTIONS *****/
+
+void	HumanA::setWeapon(Weapon type)
+{
+	_Weapon = type;
+}
+
+void	HumanA::attack(void) const
+{
+	std::cout << this->_name << " attacks with their " << _Weapon.getType() << std::endl;
 }
