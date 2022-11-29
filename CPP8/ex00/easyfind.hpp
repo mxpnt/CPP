154906a71fe20d37265c867e9fe9c76ed4fcfcb5
Maxime Pontille <maxime.pontille1@gmail.com>
2022-11-29 08:16:05 +0100

    w

--- CPP8/ex00/easyfind.hpp
+++ CPP8/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: mapontil <mapontil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 17:12:04 by mapontil          #+#    #+#             */
-/*   Updated: 2022/10/27 19:56:22 by mapontil         ###   ########.fr       */
+/*   Updated: 2022/11/29 08:14:19 by mapontil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,35 +18,44 @@
 # include <list>
 # include <vector>
 # include <exception>
+# include <algorithm>
 
-// class	Exception {
-// public:
-	class	NotFindOccur : public std::exception {
-		public:
-			virtual const char* what() const throw() {
-				return ("No occurence found");
-			}
-	};
-// };
+class	NotFindOccur : public std::exception {
+	public:
+		virtual const char* what() const throw() {
+			return ("No occurence found");
+		}
+};
+
+
+bool	equal()
+{
+
+}
 
 template <typename T>
 int	easyfind(T container, int n)
 {
 	typename T::const_iterator	it = container.begin();
 	typename T::const_iterator	ite = container.end();
-	// int	save = *container.begin();
+	// // int	save = *container.begin();
 
 
-	for (it = container.begin(); it != ite; ++it)
+	// for (it = container.begin(); it != ite; ++it)
+	// {
+	// 	if (static_cast<int>(*it) == n)
+	// 		break ;
+	// 	// save = static_cast<int>(*it);
+	// }
+	// if (it != ite && static_cast<int>(*it) == n)
+	// 	return (static_cast<int>(*it));
+	// else
+	// 	throw NotFindOccur();
+	if (std::any_of(it, ite, equal))
 	{
-		if (static_cast<int>(*it) == n)
-			break ;
-		// save = static_cast<int>(*it);
+		
 	}
-	if (it != ite && static_cast<int>(*it) == n)
-		return (static_cast<int>(*it));
-	else
-		throw NotFindOccur();
+
 }
 
 #endif
