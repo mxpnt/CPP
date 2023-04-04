666ee1811a151a8b8cba9bfaff80ca8d3a4af9c1
mxpnt <maxime.pontille1@gmail.com>
2023-04-04 18:16:39 +0200

    msg error ac

--- CPP9/ex00/main.cpp
+++ CPP9/ex00/main.cpp
@@ -23,7 +23,7 @@ int	main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cerr << "Error: wrong number of arguments: ./btc <*.txt>." << std::endl;
+		std::cerr << "Error: wrong number of arguments: ./btc <*.txt>" << std::endl;
 		return (1);
 	}
 	std::string	file = argv[1];
