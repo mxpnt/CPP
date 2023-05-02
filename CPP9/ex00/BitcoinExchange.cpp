8363dec2ba71d112068f62b6b5f1642eaf5ee2ae
mxpnt <maxime.pontille1@gmail.com>
2023-05-02 13:11:44 +0200

    std98

--- CPP9/ex00/BitcoinExchange.cpp
+++ CPP9/ex00/BitcoinExchange.cpp
@@ -106,19 +106,11 @@ void	BitcoinExchange::valid_format(size_t index)
 
 void	BitcoinExchange::valid_date(size_t index)
 {
-<<<<<<< HEAD
-	std::string	syear = inputDB[index].substr(0, 4);
-	int	year = atoi(syear.c_str());
-	std::string	smonth = inputDB[index].substr(5, 2);
-	int	month = atoi(smonth.c_str());
-	std::string sday = inputDB[index].substr(8, 2);
-=======
 	std::string	syear = inputDB[index].substr(0,4);
 	int	year = atoi(syear.c_str());
 	std::string smonth = inputDB[index].substr(5,2);
 	int	month = atoi(smonth.c_str());
 	std::string	sday = inputDB[index].substr(8,2);
->>>>>>> 1f48728 (ex00 error handle ifs.empty/fail + date too soon + invalid date if len < 13)
 	int	day = atoi(sday.c_str());
 	
 	if (month > 12 || month < 1 || day > 31 || day < 1 || year < 0)
@@ -157,21 +149,13 @@ void	BitcoinExchange::valid_value(size_t index)
 	}
 	try
 	{
-<<<<<<< HEAD
 		std::string	svalue = inputDB[index].substr(inputDB[index].find_last_of(' '), std::string::npos - 1);
 		long long int n = atoll(svalue.c_str());
-		if (n > INT_MAX)
-			throw ;
-=======
-		int	nb;
-		
-		nb = stoi(inputDB[index].substr(inputDB[index].find_last_of(' '), std::string::npos - 1));
-		if (nb > 1000)
+		if (n > 1000)
 		{
 			std::string	msg = "Error: too large value\n";
 			throw BitcoinExchange::CustomException(msg);
 		}
->>>>>>> 1f48728 (ex00 error handle ifs.empty/fail + date too soon + invalid date if len < 13)
 	}
 	catch (std::exception &e)
 	{
@@ -194,16 +178,9 @@ void	BitcoinExchange::processExchange()
 			valid_value(i);
 
 			date = inputDB[i].substr(0, inputDB[i].find_first_of(' '));
-<<<<<<< HEAD
-			char		*endPtr;
-			std::string	svalue = inputDB[i].substr(inputDB[i].find_last_of(' '), std::string::npos - 1);
-			value = strtod(svalue.c_str(), &endPtr);
-
-=======
 			std::string	svalue = inputDB[i].substr(inputDB[i].find_last_of(' '), std::string::npos - 1);
 			char *endPtr;
 			value = strtod(svalue.c_str(), &endPtr);
->>>>>>> 1f48728 (ex00 error handle ifs.empty/fail + date too soon + invalid date if len < 13)
 
 			std::map<std::string, float>::iterator	it_data = dataMap.lower_bound(date);
 			std::map<std::string, float>::iterator	it_save = dataMap.begin();
@@ -233,15 +210,9 @@ void	BitcoinExchange::mapping_dataDB()
 	while (i < dataDB_size)
 	{
 		std::string	date = dataDB[i].substr(0, dataDB[i].find_first_of(','));
-<<<<<<< HEAD
-		char		*endPtr;
-		std::string	svalue = dataDB[i].substr(dataDB[i].find_first_of(',') + 1, std::string::npos - 1);
-		double		value = strtod(svalue.c_str(), &endPtr);
-=======
 		std::string	svalue = dataDB[i].substr(dataDB[i].find_first_of(',') + 1, std::string::npos - 1);
 		char *endPtr;
 		double	value = strtod(svalue.c_str(), &endPtr);
->>>>>>> 1f48728 (ex00 error handle ifs.empty/fail + date too soon + invalid date if len < 13)
 		this->dataMap[date] = value;
 		++i;
 	}
